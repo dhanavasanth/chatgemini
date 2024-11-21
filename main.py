f6694e94d50fe6ca70d3e0162d58988d38b1131a
DHANA VASANTH <117557948+dhanavasanth@users.noreply.github.com>
2024-11-21 09:54:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="GenAI", page_icon=":robot_face:", layout="wide")
 st.title("Google gemini chatbot")
 
 
-api_key = st.secrets["gemini_api_key"]
+api_key = "AIzaSyAGrh87fh-YjmctONh_g3uei-oY4yMIDw0"
 
 genai.configure(api_key = api_key)
 
